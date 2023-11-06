4a57dd18bc6ef1f67e6e308a0a9f29877d6f590d
Cursed Pakel <92992500+pakelcomedy@users.noreply.github.com>
2023-11-06 23:50:03 +0700

    Add files via upload

---
+++ ConeArea/src/conearea/HitungLuasKerucut.java
@@ -0,0 +1,20 @@
+package conearea;
+import java.util.Scanner;
+
+public class HitungLuasKerucut {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jari-jari kerucut (r): ");
+        double jariJari = input.nextDouble();
+        System.out.print("Masukkan tinggi kerucut (h): ");
+        double tinggi = input.nextDouble();
+        double luasKerucut = hitungLuasKerucut(jariJari, tinggi);
+
+        System.out.println("Luas permukaan kerucut adalah: " + luasKerucut);
+    }
+    public static double hitungLuasKerucut(double r, double h) {
+        double sisiMiring = Math.sqrt(r * r + h * h);
+        double luasKerucut = Math.PI * r * (r + sisiMiring);
+        return luasKerucut;
+    }
+}
